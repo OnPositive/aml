9775036195417376f1c51a4c5daebbfee392552f
Petrochenko Pavel <petrochenko.pavel.a@gmail.com>
2016-11-04 08:46:20 +0700

    small update.

--- org.aml.registry/src/main/java/org/aml/registry/model/ItemDescription.java
+++ org.aml.registry/src/main/java/org/aml/registry/model/ItemDescription.java
@@ -1,7 +1,6 @@
 package org.aml.registry.model;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.MalformedURLException;

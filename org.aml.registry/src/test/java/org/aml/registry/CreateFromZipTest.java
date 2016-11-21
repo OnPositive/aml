56f290afcbd3887c686241c3bf1ce290d42a4ef8
Petrochenko Pavel <petrochenko.pavel.a@gmail.com>
2016-11-21 12:32:11 +0700

    Interesting what happens.

--- org.aml.registry/src/test/java/org/aml/registry/CreateFromZipTest.java
+++ org.aml.registry/src/test/java/org/aml/registry/CreateFromZipTest.java
@@ -11,8 +11,10 @@ public class CreateFromZipTest extends TestCase{
 	public void test0(){
 		Registry registry = RegistryManager.getInstance().getDefault();		
 		System.out.println("Registry aquired");
+		
 		registry.items().forEach(x->{
 			TopLevelModel mod=x.resolve();
+			System.out.println(x.getLocation());
 			TestCase.assertTrue(mod!=null);
 		});
 		TestCase.assertTrue(registry.items().size()>400);
